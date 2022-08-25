5dd3902d76f583df54252a12399aa73437cf4b70
yarraradha <43906675+yarraradha@users.noreply.github.com>
2022-08-25 17:49:06 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,4 +1,8 @@
 variable "project_id" {
-  description = "(Required) Project ID where bucket is created"
+  description = "(Required) The ID of the project in which e resource belongs. If it is not provided, the provider project is used"
+  type        = string
+}
+variable "dashboard_name" {
+  description = "(Required) A short name or phrase used to identify the policy in dashboards, notifications, and incidents. To avoid confusion, don't use the same display name for multiple policies in the same project. The name is limited to 512 Unicode characters."
   type        = string
 }
